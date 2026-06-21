db3d205729b00d6e45ca33f5573c5ed05b056d69
bagnatosan <bagnatosan@gmail.com>
2026-06-20 23:33:51 -0300

    Cambio de index y layout
    Campana solo si esta autenticado
    
    en index metricas  de la heuristica dudosa

--- edq/obj/Debug/net10.0/edq.AssemblyInfo.cs
+++ edq/obj/Debug/net10.0/edq.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("edq")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc2154d94791d462b65db3d8730190ed8e83819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ec9190cd55a2b7879141d2537cbf2e51ef39bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("edq")]
 [assembly: System.Reflection.AssemblyTitleAttribute("edq")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
